daa0ed67e2dda22656d9d3d9b995d0e3ad0accaa
Yo Name <yoemail@iu.edu>
2025-10-09 11:50:26 -0400

    Week7NotesIDK

---
+++ week7.2/script.js
@@ -0,0 +1,13 @@
+
+console.log("Hello, World!");
+console.log("This is a sample JavaScript file.");
+
+// String Variable //
+
+let studentName="Johnathan";
+console.log(studentName);
+console.log("the student name is :", studentName);
+
+let studentname = "John";
+console.log(studentname);
+console.log("the student name is :", studentname);
