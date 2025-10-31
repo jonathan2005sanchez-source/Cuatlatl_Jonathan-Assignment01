cb76554194ed090ab307e6b7bf6f63bfe781587a
Yo Name <yoemail@iu.edu>
2025-10-31 18:47:26 -0400

    NEW Stuff

---
+++ Z_StudentDASHBOARD/script.js
@@ -0,0 +1,35 @@
+document.write("word")
+
+let student = {
+    {name: "Robert", grade: 91},
+    {name: "lilith", grade: 91},
+    {name: "John", grade: 30},
+    {name: "Gary G.", grade: 30},
+    {name: "Jennifer", grade: 91},
+};
+
+//console.log(student.length)
+
+let output = "<ul>";
+
+for(let i = 0; i < student.length; i++) {
+
+    let status = "";
+
+    if(student[i].grade >= 50){
+        status = "Passed"
+    } else {
+        status = "failed"
+    }
+
+    output += "<li>" + student[i].name + "- Grade: " + student[i].grade + "-" + status + "</li>";
+
+    console.log{"student:", student[i].name};
+    console.log{"grade", student[i].grade}
+    console.log{"status", status}
+
+}
+
+output += "</ul>"
+
+document.getElementById("studentList").innerHTML = output;
